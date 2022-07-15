908ed6d39fb761a2fb355abd4de7a6d673aebe3d
Andrey Z <andrey@mail.ru>
2022-07-15 22:19:31 +0300

    07.15.2022

---
+++ 004/Program.cs
@@ -0,0 +1,25 @@
+﻿//По заданному с клавиатуры номеру дня недели вывести его название
+int dayOfWeek;//camelCase 
+//int DayOfWeek;//PascalCase
+//dayOfWeek=Convert.ToInt32(Console.ReadLine());
+dayOfWeek=Convert.ToInt32(Console.ReadLine());
+
+//if (dayOfWeek==1) System.Console.WriteLine("Monday");    
+//if (dayOfWeek==2) System.Console.WriteLine("Tuesday");
+        
+
+switch(dayOfWeek)
+{
+    case 1:
+        System.Console.WriteLine("Monday");
+        break;
+    case 2:
+        System.Console.WriteLine("Tuesday");
+        break;
+    case 3:
+        System.Console.WriteLine("Wednesday");
+        break;
+    default:
+        System.Console.WriteLine("Wrong!");
+        break;
+}
---
+++ 032/Program.cs
@@ -0,0 +1,45 @@
+﻿//Задать массив из 8 целых элементов и вывести их на экран
+int N=8;
+int[] a=new int[N];
+Random random=new Random();//псевдослучайные числа
+for(int i=0;i<a.Length;i++)
+    a[i]=random.Next(0,11);
+
+
+
+
+for(int i=0;i<a.Length;i++)
+    System.Console.Write($"{a[i],4}");
+
+
+
+
+
+
+
+
+
+
+/*//значимые типы (value types)
+int a=3;
+double b=4;
+float c;
+bool bl;
+c=3.14F;
+
+
+//ссылочные типы (reference types)
+string s=null;
+s="Hellowsadfsadfasdfasdf";
+string s1=s;
+int[] ar;
+Random random;
+random=new Random();
+Random random1;
+random1=random;
+
+ar=new int[20000];
+int[] ar2;
+ar2=ar;
+
+*/
