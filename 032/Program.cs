9865f434d78f413abb787897b3b220a537bc2ccf
Andrey Z <andrey@mail.ru>
2022-08-02 22:09:25 +0300

    02.08.2022

--- 032/Program.cs
+++ 032/Program.cs
@@ -1,15 +1,30 @@
 ﻿//Задать массив из 8 целых элементов и вывести их на экран
-int N=8;
-int[] a=new int[N];
-Random random=new Random();//псевдослучайные числа
-for(int i=0;i<a.Length;i++)
-    a[i]=random.Next(0,11);
+
+//Инициализация переменных
+int[] a;
+
+//Ввод данных
+Init(out a,max:100);
+//Обработка данных
+
+//Вывод результатов
+Print(a);
 
 
+void Init(out int[] a,int Length=10,int min=0,int max=10)
+{
+    a=new int[Length];
+    Random random=new Random();//псевдослучайные числа
+    for(int i=0;i<a.Length;i++)
+            a[i]=random.Next(min,max+1);
+}
 
 
-for(int i=0;i<a.Length;i++)
-    System.Console.Write($"{a[i],4}");
+void Print(int[] a)
+{
+    for(int i=0;i<a.Length;i++)
+        System.Console.Write($"{a[i],4}");
+}
 
 
 
--- 034/Program.cs
+++ 034/Program.cs
@@ -1,31 +1,58 @@
 ﻿//Задать массив из 12 элементов, заполненных числами из [-9, 9]. 
 //Найти сумму положительных/отрицательных элементов массива
 
+
+Run();
+
+
+void Run()
+{
 int N=3;
-int[] a=new int[N];
-Random random=new Random();//псевдослучайные числа
-for(int i=0;i<a.Length;i++)
-{
-    a[i]=random.Next(-9,10);
+int[] a;
+Init(out a,12,-9,9);
+int sumNegative,sumPositive;
+Solve(a,out sumPositive,out sumNegative);
+Print(a);
+//System.Console.WriteLine();
+System.Console.WriteLine($"\nСумма положительных:{sumPositive}");
+System.Console.WriteLine($"Сумма отрицательных:{sumNegative}");
 }
-int sumPositive=0;
-for(int i=0;i<a.Length;i++)
+
+
+
+
+
+
+
+
+void Solve(int[] a,out int sumPositive,out int sumNegative)
 {
+    sumPositive=0;
+    sumNegative=0;
+    for(int i=0;i<a.Length;i++)
+    {
     if (a[i]>0)
     {
         sumPositive=sumPositive+a[i];
     }
-}
-int sumNegative=0;
-for(int i=0;i<a.Length;i++)
-{
     if (a[i]<0)
     {
         sumNegative+=a[i];
     }
+    }    
+    
 }
-for(int i=0;i<a.Length;i++)
-    System.Console.Write($"{a[i],4}");
-//System.Console.WriteLine();
-System.Console.WriteLine($"\nСумма положительных:{sumPositive}");
-System.Console.WriteLine($"Сумма отризательных:{sumNegative}");
+void Init(out int[] a,int Length=10,int min=0,int max=10)
+{
+    a=new int[Length];
+    Random random=new Random();//псевдослучайные числа
+    for(int i=0;i<a.Length;i++)
+            a[i]=random.Next(min,max+1);
+}
+
+
+void Print(int[] a)
+{
+    for(int i=0;i<a.Length;i++)
+        System.Console.Write($"{a[i],4}");
+}
